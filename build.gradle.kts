92671aa01591360db78a622cf1c7426823106df6
mikrise2 <mikrise2@mail.ru>
2023-03-14 18:03:55 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+val telegramBotApiVersion : String by project
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("dev.inmo:tgbotapi:$telegramBotApiVersion")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
