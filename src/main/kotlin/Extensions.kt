6b720762e4d0b3c63bb9af7f513822e87f8a32a6
mikrise2 <mikrise2@mail.ru>
2023-03-15 00:41:41 +0100

    help, changeLanguage, bot commands

---
+++ src/main/kotlin/Extensions.kt
@@ -0,0 +1,84 @@
+import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
+import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.dataButton
+import dev.inmo.tgbotapi.extensions.utils.usernameChatOrThrow
+import dev.inmo.tgbotapi.requests.send.SendTextMessage
+import dev.inmo.tgbotapi.types.BotCommand
+import dev.inmo.tgbotapi.types.buttons.InlineKeyboardMarkup
+import dev.inmo.tgbotapi.types.chat.Chat
+import dev.inmo.tgbotapi.utils.matrix
+import dev.inmo.tgbotapi.utils.row
+import java.util.*
+
+fun createIfNotExist(chat: Chat): User {
+    if (!users.containsKey(chat.id)) {
+        users[chat.id] = User(chat.usernameChatOrThrow().username?.username ?: "")
+    }
+    return users[chat.id]!!
+}
+
+enum class Language(val languageName: String, val locale: Locale) {
+    RUSSIAN("Russian", Locale("ru")), ENGLISH("English", Locale("en")), GERMAN(
+        "German",
+        Locale("de")
+    )
+}
+
+private val languages = Language.values().map { it.languageName }
+
+data class User(
+    val username: String,
+    var preferredName: String = "user",
+    var language: Language = Language.ENGLISH,
+    val friends: List<String> = mutableListOf()
+)
+
+fun checkLanguage(language: String) = language in languages
+
+fun chooseLanguageButtons(): InlineKeyboardMarkup {
+    return InlineKeyboardMarkup(matrix {
+        for (language in languages) {
+            row {
+                dataButton(language, language)
+            }
+        }
+    })
+}
+
+fun getCommands(chat: Chat?): List<BotCommand> {
+    var bundle = ResourceBundle.getBundle("commands", Language.ENGLISH.locale)
+    chat?.let {
+        val user = createIfNotExist(it)
+        bundle = ResourceBundle.getBundle("commands", user.language.locale)
+    }
+    return listOf(
+        "help",
+        "javascript",
+        "translate",
+        "add_friend",
+        "share_location",
+        "show_friends",
+        "change_name",
+        "change_language"
+    ).map { BotCommand(it, bundle.getString(it)) }
+}
+
+fun getSendText(
+    chat: Chat,
+    bundleKey: String,
+    vararg params: String
+): SendTextMessage {
+    val user = createIfNotExist(chat)
+    val bundle = ResourceBundle.getBundle("messages", user.language.locale)
+    return SendTextMessage(chat.id, bundle.getString(bundleKey).format(params))
+}
+
+suspend fun BehaviourContext.sendMessage(
+    chat: Chat,
+    bundleKey: String,
+    vararg params: String
+) {
+    val user = createIfNotExist(chat)
+    val bundle = ResourceBundle.getBundle("messages", user.language.locale)
+    sendTextMessage(chat.id, bundle.getString(bundleKey).format(params))
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,74 +1,69 @@
 import dev.inmo.tgbotapi.bot.ktor.telegramBot
-import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
+import dev.inmo.tgbotapi.extensions.api.bot.setMyCommands
+import dev.inmo.tgbotapi.extensions.api.send.sendMessage
 import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
-import dev.inmo.tgbotapi.extensions.behaviour_builder.expectations.waitText
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
 import dev.inmo.tgbotapi.extensions.behaviour_builder.expectations.waitDataCallbackQuery
+import dev.inmo.tgbotapi.extensions.behaviour_builder.expectations.waitText
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
-import dev.inmo.tgbotapi.extensions.utils.types.buttons.dataButton
 import dev.inmo.tgbotapi.requests.send.SendTextMessage
 import dev.inmo.tgbotapi.types.IdChatIdentifier
-import dev.inmo.tgbotapi.types.buttons.InlineKeyboardMarkup
-import dev.inmo.tgbotapi.utils.matrix
-import dev.inmo.tgbotapi.utils.row
+import dev.inmo.tgbotapi.types.chat.Chat
 import io.ktor.util.*
 import kotlinx.coroutines.flow.first
-import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 
-private val users = ConcurrentHashMap<IdChatIdentifier, User>()
+internal val users = ConcurrentHashMap<IdChatIdentifier, User>()
 
-enum class Language(val languageName: String, val locale: Locale) {
-    RUSSIAN("Russian", Locale("ru")), ENGLISH("English", Locale("en")), GERMAN(
-        "German",
-        Locale("de")
-    )
-}
-
-private val languages = Language.values().map { it.languageName }
-
-data class User(var preferredName: String = "user", var language: Language = Language.ENGLISH)
-
-fun checkLanguage(language: String) = language in languages
-
-fun chooseLanguageButtons(): InlineKeyboardMarkup {
-    return InlineKeyboardMarkup(matrix {
-        for (language in languages) {
-            row {
-                dataButton(language, language)
-            }
-        }
-    })
+suspend fun BehaviourContext.getLanguage(chat: Chat): Language {
+    var language = waitDataCallbackQuery(
+        SendTextMessage(
+            chat.id,
+            "Choose your language:",
+            replyMarkup = chooseLanguageButtons()
+        )
+    ).first().data
+    while (!checkLanguage(language)) {
+        language = waitDataCallbackQuery(
+            SendTextMessage(
+                chat.id,
+                "Incorrect language, Please choose correct one:",
+                replyMarkup = chooseLanguageButtons()
+            )
+        ).first().data
+    }
+    return Language.valueOf(language.toUpperCasePreservingASCIIRules())
 }
 
 suspend fun BehaviourContext.start() {
     onCommand("start") {
-        var language = waitDataCallbackQuery(
-            SendTextMessage(
-                it.chat.id,
-                "Choose your language:",
-                replyMarkup = chooseLanguageButtons()
-            )
-        ).first().data
-        while (!checkLanguage(language)) {
-            language = waitDataCallbackQuery(
-                SendTextMessage(
-                    it.chat.id,
-                    "Incorrect language, Please choose correct one:",
-                    replyMarkup = chooseLanguageButtons()
-                )
-            ).first().data
-        }
-        users[it.chat.id] =
-            User(language = Language.valueOf(language.toUpperCasePreservingASCIIRules()))
+        val language = getLanguage(it.chat)
+        createIfNotExist(it.chat)
         val user = users[it.chat.id]!!
-        val bundle = ResourceBundle.getBundle("messages", user.language.locale)
+        user.language = language
+        setMyCommands(getCommands(it.chat))
         val preferredName = waitText(
-            SendTextMessage(it.chat.id, bundle.getString("chooseName"))
+            getSendText(it.chat, "chooseName")
         ).first().text
         user.preferredName = preferredName
-        sendTextMessage(it.chat.id, bundle.getString("yourNameIs").format(user.preferredName))
+        sendMessage(it.chat, "yourNameIs", user.preferredName)
+    }
+}
+
+suspend fun BehaviourContext.help() {
+    onCommand("help") {
+        sendMessage(it.chat, "help")
+    }
+}
+
+suspend fun BehaviourContext.changeLanguage() {
+    onCommand("change_language") {
+        val user = createIfNotExist(it.chat)
+        val language = getLanguage(it.chat)
+        user.language = language
+        setMyCommands(getCommands(it.chat))
+        sendMessage(it.chat, "languageChangedSuccessful")
     }
 }
 
@@ -76,5 +71,8 @@ suspend fun main() {
     val bot = telegramBot(System.getenv("TOKEN"))
     bot.buildBehaviourWithLongPolling {
         start()
+        help()
+        changeLanguage()
+        setMyCommands(getCommands(null))
     }.join()
 }
